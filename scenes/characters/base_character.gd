b3b061998df8f876ca507398f739c48706b2c9d1
Ryan <rmbackman@gmail.com>
2025-07-25 18:38:14 -0700

    import player model, implement animation states

--- scenes/characters/base_character.gd
+++ scenes/characters/base_character.gd
@@ -1,28 +1,27 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
 var bullet = load("res://scenes/entities/Bullet.tscn")
+var defaultCharacter = preload("res://assets/models/characters/fella.glb")
 var instance
 
 @onready var model = $CharacterModel
+@onready var anim_player = $CharacterModel/fella/AnimationPlayer
 @onready var camera_target = $CameraController/CameraTarget
 @onready var gun_barrel = $CharacterModel/DefaultWeapon/RayCast3D
-
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	elif Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = Global.JUMP_VELOCITY
 
 	handle_input(delta)
-	
+	update_state()
+	update_animation()
 	move_and_slide()
 	
 func handle_input(delta: float) -> void:
-		# handle directional input
+	# handle directional input
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 
 	if input_dir.length() > 0:
@@ -31,27 +30,58 @@ func handle_input(delta: float) -> void:
 		var rotation_basis = Basis(Vector3.UP, camera_target.rotation.y) * direction
 		direction = rotation_basis.normalized()
 
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * Global.SPEED
+		velocity.z = direction.z * Global.SPEED
 
 		# rotate model to input direction
 		var target_rotation_y = atan2(-direction.x, -direction.z)
 		model.rotation.y = lerp_angle(model.rotation.y, target_rotation_y, delta * 15)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, Global.SPEED)
+		velocity.z = move_toward(velocity.z, 0, Global.SPEED)
 		
 	if Input.is_action_just_pressed("shoot"):
 		shoot(delta)
+
+func update_state() -> void:
+	if Global.player_health <= 0:
+		Global.player_state = Global.PlayerState.DEATH
+		return
 		
+	if Input.is_action_pressed("shoot"):
+		Global.player_state = Global.PlayerState.SHOOTING
+		return
+
+	if not is_on_floor():
+		if velocity.y > 0:
+			Global.player_state = Global.PlayerState.JUMP_IDLE_UP
+		else:
+			Global.player_state = Global.PlayerState.FALLING
+		return
+
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	if input_dir.length() > 0:
+		if Input.is_action_pressed("aim"):
+			Global.player_state = Global.PlayerState.AIM_RUNNING
+		else:
+			Global.player_state = Global.PlayerState.RUNNING
+		return
+
+	if Input.is_action_pressed("aim"):
+		Global.player_state = Global.PlayerState.AIM_IDLE
+		return
+
+	Global.player_state = Global.PlayerState.IDLE
+
+func update_animation():
+	var anim_attr = Global.state_dict.get(Global.player_state)
+	if anim_player and anim_player.has_animation(anim_attr.anim_name):
+		if anim_player.current_animation != anim_attr.anim_name:
+			anim_player.play(anim_attr.anim_name, anim_attr.blend_time, anim_attr.speed)
+	else:
+		push_warning("animation not found: " + anim_attr.anim_name)
+
 func shoot(_delta: float) -> void:
-	#if !gun_anim.is_playing():
-		#gun_anim.play("shoot")
-		#instance = bullet.instantiate()
-		#instance.position = gun_barrel.global_position
-		#instance.transform.basis = gun_barrel.global_transform.basis
-		#get_parent().add_child(instance)
-		
 	instance = bullet.instantiate()
 	instance.position = gun_barrel.global_position
 	instance.transform.basis = gun_barrel.global_transform.basis
--- scripts/global.gd
+++ scripts/global.gd
@@ -1,14 +1,55 @@
 extends Node
 
+# CONSTANTS
+# ---------
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const JUMP_START_DURATION = 0.1
+
+# STATE
+# -----
+enum PlayerState {
+	IDLE,
+	AIM_IDLE,
+	
+	RUNNING,
+	AIM_RUNNING,
+	
+	JUMP_START,
+	JUMP_IDLE_UP,
+	JUMP_IDLE_DOWN,
+	JUMP_LAND,
+	JUMP_TO_FALL,
+	
+	FALLING,
+	
+	SHOOTING,
+	
+	HURT,
+	DEATH,
+	GAINING_LIFE,
+	LOSING_LIFE
+}
+
+var state_dict = {
+	PlayerState.IDLE: {"anim_name": "Idle", "blend_time": 0.2, "speed": 1.0},
+	PlayerState.AIM_IDLE: {"anim_name": "Aim Idle", "blend_time": 0.2, "speed": 1.0},
+	PlayerState.RUNNING: {"anim_name": "Running", "blend_time": 0.15, "speed": 1.0},
+	PlayerState.AIM_RUNNING: {"anim_name": "Running Aim", "blend_time": 0.15, "speed": 1.0},
+	PlayerState.JUMP_START: {"anim_name": "Jump Start", "blend_time": 0.05, "speed": 4},
+	PlayerState.JUMP_IDLE_UP: {"anim_name": "Jump Up Idle", "blend_time": 2, "speed": 0.7},
+	PlayerState.JUMP_LAND: {"anim_name": "Jump Land", "blend_time": 0, "speed": 1.0},
+	PlayerState.FALLING: {"anim_name": "Fall Idle", "blend_time": 0.2, "speed": 1.0},
+	PlayerState.SHOOTING: {"anim_name": "Shoot", "blend_time": 0.3, "speed": 1.0},
+	PlayerState.HURT: {"anim_name": "Hurt", "blend_time": 0.2, "speed": 1.0},
+	PlayerState.DEATH: {"anim_name": "Death", "blend_time": 0.5, "speed": 1.0}
+}
+
+# STATS
+# -----
+var player_health = 3
+var player_state = PlayerState.IDLE
+
+# RENDERING
+# ---------
 var main_scene
-var player
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
